9895cd82bf7782cfb21413af386c17b32b390171
EK <zxjq97@gmail.com>
2024-05-26 14:34:30 +0700

    update

--- setup.py
+++ setup.py
@@ -1,38 +1,20 @@
-﻿import io
-from os.path import abspath, dirname, join
+﻿from os.path import abspath, dirname, join
 from setuptools import find_packages, setup
 
+working_dir = abspath(dirname(__file__))
 
-HERE = dirname(abspath(__file__))
-LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
-DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
-    'README.rst'
-])
+with open(join(working_dir,'README.md'),encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-  name = 'lnwza',      
-  packages = ['lnwza'], 
-  version = '0.0.1', 
-  license='MIT', 
-  description = 'lnwza by hexs',
-  long_description=DESCRIPTION,
-  author = 'zxjq',                 
-  author_email = 'zxjq97@gmail.com',     
-  url = 'https://github.com/hexs/lnwza',  
-  download_url = 'https://github.com/hexs/lnwza/archive/v0.0.1.zip',  
-  keywords = ['hexs', 'lnwza'],
-  classifiers=[
-    'Development Status :: 3 - Alpha',     
-    'Intended Audience :: Education',     
-    'Topic :: Utilities',
-    'License :: OSI Approved :: MIT License',   
-    'Programming Language :: Python :: 3',      
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-  ],
+    name='lnwza',
+    version='0.0.1',
+    license='MIT',
+    description='lnwza by hexs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='hexs',
+    author_email='zxjq97@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
 )
